f3da8fcdbf19b13384a028a4b03b64f348d0dccf
seo1016 <seoseunghun3@gmail.com>
2024-11-11 21:00:23 +0900

    Create SuccessFragment View

--- app/src/main/java/com/toss/pepero/GameOverFragment.kt
+++ app/src/main/java/com/toss/pepero/GameOverFragment.kt
@@ -5,17 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.toss.pepero.databinding.FragmentFailureBinding
+import com.toss.pepero.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
 
-    private lateinit var binding: FragmentFailureBinding
+    private lateinit var binding: FragmentGameOverBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentFailureBinding.inflate(inflater, container, false)
+        binding = FragmentGameOverBinding.inflate(inflater, container, false)
 
         return binding.root
     }
